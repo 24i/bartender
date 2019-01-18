3e163f7522d562fe954ca25ce63404353533cc4a
Bas Tuijnman <bas.tuijnman@24imedia.com>
2019-01-18 17:47:06 +0100

    Add server

--- pi/server.py
+++ pi/server.py
@@ -1,15 +1,23 @@
 from gpiozero import PWMLED
 from time import sleep
+from socket import *
+import http.server
 
 red = PWMLED(16)
 green = PWMLED(20)
 blue = PWMLED(21)
 
-from socket import *
 cs = socket(AF_INET, SOCK_DGRAM)
 cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
 cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
 
+with socketserver.TCPServer(("", 8000), Handler) as httpd:
+    print("serving at port", 8000)
+    httpd.serve_forever()
+
 while True:
-    cs.sendto('This is a test', ('255.255.255.255', 54545))
+
+    print("UDP signal")
+    cs.sendto('bartender_broadcast', ('255.255.255.255', 54545))
+
     sleep(2)
