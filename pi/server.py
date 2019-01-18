7c99c38a285a227f60209a83021bb4b6407ac32b
Bas Tuijnman <bas.tuijnman@24imedia.com>
2019-01-18 17:34:10 +0100

    Add UPD test

--- pi/server.py
+++ pi/server.py
@@ -5,12 +5,11 @@ red = PWMLED(16)
 green = PWMLED(20)
 blue = PWMLED(21)
 
+from socket import *
+cs = socket(AF_INET, SOCK_DGRAM)
+cs.setsockopt(SOL_SOCKET, SO_REUSEADDR, 1)
+cs.setsockopt(SOL_SOCKET, SO_BROADCAST, 1)
+
 while True:
-    red.on()
-    green.on()
-    blue.on()
-    sleep(1)
-    red.off()
-    green.off()
-    blue.off()
-    sleep(1)
+    cs.sendto('This is a test', ('255.255.255.255', 54545))
+    sleep(2)
