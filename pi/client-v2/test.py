8e89b5ee325df8c3c5f8827aab0cf0af1a68962d
Bas Tuijnman <bas.tuijnman@24i.com>
2022-08-31 15:08:09 +0200

    Add button test

--- pi/client-v2/test.py
+++ pi/client-v2/test.py
@@ -1,6 +1,6 @@
 from asyncio import events
 from threading import Event
-from gpiozero import RotaryEncoder
+from gpiozero import RotaryEncoder,Button
 
 done = Event()
 rotor = RotaryEncoder(21, 9)
@@ -9,4 +9,12 @@ def test():
     print("ROTATE\n")
 
 rotor.when_rotated = test
+
+def say_hello():
+    print("Hello!")
+
+button = Button(8)
+
+button.when_pressed = say_hello
+
 done.wait()
